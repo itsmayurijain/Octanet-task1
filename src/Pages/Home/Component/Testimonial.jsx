d7ce60fe01ddd541abe5cafaabf4345c0665b715
itsmayurijain <mayurijainsongir@gmail.com>
2024-07-08 20:16:34 +0530

    website updated

--- src/Pages/Home/Component/Testimonial.jsx
+++ src/Pages/Home/Component/Testimonial.jsx
@@ -2,6 +2,27 @@ import React from 'react'
 import Logo1 from '../../Assets/logo 1.avif'
 import Logo2 from '../../Assets/logo 2.avif'
 import Logo3 from '../../Assets/logo 3.avif'
+
+const data =[
+    {
+        image: Logo1,
+        description:'Experience the perfect blend of technology and fashion with our smartwatches.',
+        name:'Charlotte Hale',
+        post:'Creative Manager',
+    },
+    {
+        image: Logo2,
+        description:'Experience the perfect blend of technology and fashion with our smartwatches.',
+        name:'John Allenden',
+        post:'Creative Manager',
+    },
+    {
+        image: Logo3,
+        description:'Experience the perfect blend of technology and fashion with our smartwatches.',
+        name:'One Allenden',
+        post:'Creative Manager',
+    },
+]
 export const Testimonial = () => {
     return (
         <div>
@@ -12,49 +33,27 @@ export const Testimonial = () => {
                     </div>
                     <div className='my-12'>
                         <div className="grid grid-cols-1 lg:grid-cols-3 gap-4">
-                            <div className='bg-black-200 rounded-2xl'>
-                                <div className='text-center py-4'>
-                                    <img src={Logo1} className='rounded-full m-auto w-[100px]'/>
-                                </div>
-                                <div className='text-center px-6'>
-                                    <p className='text-white font-medium text-sm'>Experience the perfect blend of technology and fashion with our smartwatches.</p>
-                                </div>
-                                <div className='text-center my-4'>
-                                    <h2 className='text-primary font-semibold text-lg'> Charlotte Hale</h2>
-                                </div>
-                                <div className='text-center my-5'>
-                                    <h2 className='text-white font-normal text-xs'>Creative Manager</h2>
-                                </div>
-                            </div>
-                            <div className='bg-black-200 rounded-2xl'>
-                                <div className='text-center py-4'>
-                                    <img src={Logo2} className='rounded-full m-auto w-[100px]'/>
-                                </div>
-                                <div className='text-center px-6'>
-                                    <p className='text-white font-medium text-sm'>Experience the perfect blend of technology and fashion with our smartwatches.</p>
-                                </div>
-                                <div className='text-center my-4'>
-                                    <h2 className='text-primary font-semibold text-lg'> John Allenden</h2>
-                                </div>
-                                <div className='text-center my-5'>
-                                    <h2 className='text-white font-normal text-xs'>Creative Manager</h2>
-                                </div>
-                            </div>
-                            <div className='bg-black-200 rounded-2xl'>
-                                <div className='text-center py-4'>
-                                    <img src={Logo3} className='rounded-full m-auto w-[100px]'/>
-                                </div>
-                                <div className='text-center px-6'>
-                                    <p className='text-white font-medium text-sm'>Experience the perfect blend of technology and fashion with our smartwatches.</p>
-                                </div>
-                                <div className='text-center my-4'>
-                                    <h2 className='text-primary font-semibold text-lg'> One Allenden</h2>
-                                </div>
-                                <div className='text-center my-5'>
-                                    <h2 className='text-white font-normal text-xs'>Creative Manager</h2>
-                                </div>
-                            </div>
-
+                                 {
+                                     data.map((item) => {
+                                        return (
+                                            <div className='bg-black-200 rounded-2xl'>
+                                            <div className='text-center py-4'>
+                                                <img src={item.image} className='rounded-full m-auto w-[100px]'/>
+                                            </div>
+                                            <div className='text-center px-6'>
+                                                <p className='text-white font-medium text-sm'>{item.description}</p>
+                                            </div>
+                                            <div className='text-center my-4'>
+                                                <h2 className='text-primary font-semibold text-lg'> {item.name}</h2>
+                                            </div>
+                                            <div className='text-center my-5'>
+                                                <h2 className='text-white font-normal text-xs'>{item.post}</h2>
+                                            </div>
+                                        </div> 
+                                        )
+                                    }
+                                )
+                            }
                         </div>
                     </div>
                 </div>
